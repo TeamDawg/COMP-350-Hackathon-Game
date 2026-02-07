2fa8f747a29a156587ac80c5f26f1a3cd226b48c
Andrew Chung <188700951+AndrewLChung@users.noreply.github.com>
2026-02-07 08:49:39 -0500

    Player character
    - Scene with moveable character
    - Added input map's actions

---
+++ hackathon-2026-ideas-350/scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$Jump.play()
+
+	# Get the input direction (-1, 0, 1)
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# Play animations & add the gravity.
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+		velocity += get_gravity() * delta
+	
+	# Handle the movement/deceleration.
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
