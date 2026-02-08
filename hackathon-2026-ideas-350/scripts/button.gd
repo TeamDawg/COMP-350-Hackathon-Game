f507715aa7457dce62cccb3fe2f3b74bf4c930bc
Mark Zheng <mzheng03@wesleyan.edu>
2026-02-08 14:48:31 -0500

    Add reset, Conclusion Slide, instructions

--- hackathon-2026-ideas-350/scripts/button.gd
+++ hackathon-2026-ideas-350/scripts/button.gd
@@ -2,18 +2,21 @@ extends Area2D
 
 @onready var animate_button: AnimatedSprite2D = $animateButton
 @onready var player: CharacterBody2D = %player
+@onready var animation_player: AnimationPlayer = $"../howPlatform/PlatformTrigger/AnimationPlayer"
 
 var pressed : bool = false
-
 func _on_body_entered(body: CharacterBody2D) -> void:
 	if (body == player):
 		if (pressed == false):
 			animate_button.frame = 0
+			animation_player.play("RESET")
 			pressed = true
+			
 	
-
+	
 func _on_body_exited(body: CharacterBody2D) -> void:
 	if (body == player):
 		if (pressed == true):
 			animate_button.frame = 1
 			pressed = false
+		
